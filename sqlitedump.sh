c27faaf7e9628ddb527a3f7b708265f99959b4ca
Federico Nafria <federiconafria@gmail.com>
2023-08-09 14:17:14 +0000

    Use prinf instead of echo -e

--- sqlitedump.sh
+++ sqlitedump.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 DB=$1
 # get list of tables as a single column without migration_log
 TABLES=$(sqlite3 $DB .tables | sed -r 's/(\S+)\s+(\S)/\1\n\2/g' | grep -v migration_log)
@@ -9,8 +9,8 @@ done
 
 # output sql to insert data into all target tables
 for t in $TABLES; do
-    echo -e ".headers on"
-    echo -e ".mode insert $t\nselect * from $t;"
+    printf '.headers on\n'
+    printf '.mode insert %s\nselect * from %s;\n' "$t" "$t"
 # sed is for adding a backslash to \t,\r,\n,\t & \" so they are properly escaped for MySQL
 # awk is to convert double-quotes to backticks to properly quote reserved column names for MySQL
 # TODO: add ability to add backticks for MYSQL_RESERVED_WORDS
